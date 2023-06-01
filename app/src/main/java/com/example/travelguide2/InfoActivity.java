2169bf6d5f519ae2adf04c453583ce342bf753c3
Viven7374 <vivianhuang7374@gmail.com>
2023-06-01 16:14:17 +0800

    编辑信息界面大致完成

--- app/src/main/java/com/example/travelguide2/InfoActivity.java
+++ app/src/main/java/com/example/travelguide2/InfoActivity.java
@@ -1,23 +1,32 @@
 package com.example.travelguide2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.travelguide2.dao.UserDao;
 import com.example.travelguide2.entity.User;
 import com.example.travelguide2.utils.ActivityCollector;
 import com.example.travelguide2.widget.ItemGroup;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.internal.ToolbarUtils;
 
 public class InfoActivity extends AppCompatActivity {
 
     private ItemGroup igId,igName,igDescription,igGender,igEmail,igBirthday,igCreateTime;
     private LinearLayout riPortrait;
+    Toolbar toolBar;
+
 
 
     @Override
@@ -27,7 +36,7 @@ public class InfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_info);
 
         //适用于网络请求数据量很小
-        if (android.os.Build.VERSION.SDK_INT > 9) {
+        if (Build.VERSION.SDK_INT > 9) {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
@@ -40,6 +49,7 @@ public class InfoActivity extends AppCompatActivity {
         igEmail = findViewById(R.id.ig_email);
         igBirthday = findViewById(R.id.ig_birthday);
         igCreateTime = findViewById(R.id.ig_createTime);
+        toolBar=findViewById(R.id.toolbar_edit);
 
         //获取当前登陆用户
         SharedPreferences sp = getSharedPreferences("UserInfo", Context.MODE_PRIVATE);
@@ -56,6 +66,18 @@ public class InfoActivity extends AppCompatActivity {
         igBirthday.getContentEdt().setText(user.getBirthday());
         igCreateTime.getContentEdt().setText(user.getCreate_time());
 
+        //"完成"按钮监听
+        toolBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.finish_edit:
+                        break;
+                }
+                return true;
+
+            }
+        });
         //填写简介
 //        igDescription.setOnClickListener(new View.OnClickListener() {
 //            @Override
